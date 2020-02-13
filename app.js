e46a46a59454a388500bb121315db0dae5797eab
GusGo <gus.gonzalez.15@gmail.com>
2020-02-12 23:27:32 -0500

    adding requires

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 const inquirer = require('inquirer');
-
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
